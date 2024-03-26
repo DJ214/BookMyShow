b188aa5cec9e8f12f8632be165dc970c97b3ec09
Deepanshu Jain <65814312+DJ214@users.noreply.github.com>
2024-03-26 10:56:21 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -4,11 +4,11 @@ const jwt = require("jsonwebtoken");
 module.exports = function(req,res,next){
     try{
         // [Bearer, ${localStorage.getItem('token')}]
-        console.log(req.headers.authorization);
+        //console.log(req.headers.authorization);
         const token = req.headers.authorization.split(' ')[1];
-        console.log(token);
+        //console.log(token);
         const decoded = jwt.verify(token,process.env.jwt_secret_key);
-        console.log(decoded);
+        //console.log(decoded);
         req.body.userId = decoded.userId
         
         next();
